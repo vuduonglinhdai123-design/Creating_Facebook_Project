50dd02df971786971bd80ad910041d7a7cfa6b7c
Tu Linh <linhtunguyen.1311@gmail.com>
2021-07-17 16:28:13 +0700

    change addpost form

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,25 @@
-import { Post } from "./class/Post";
-import { AddPost } from "./class/AddPost";
+// import { Post } from "./class/Post";
+// import { AddPost } from "./class/AddPost";
 
-var addPostContainer = document.querySelector('.add_post')
-var addPostForm = new AddPost()
-addPostForm.render(addPostContainer)
+//disable/enable post button
+const postbutton = document.querySelector('.postbutton')
+const postcontent = document.querySelector('.addpostcontent')
+postbutton.disabled = true
+postcontent.onkeyup = () => {
+    if (postcontent.value == '') {
+        postbutton.disabled = true
+    } else {
+        postbutton.disabled = false
+    }
+}
+
+// Add Post
+function addPost(user, content,) {
+    db.collection('post').doc().set({
+        name: user,
+        content: content,
+        deleted: false,
+        likes: 0,
+        timestamp: Date.now()
+    })
+}
